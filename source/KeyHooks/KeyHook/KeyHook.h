66d8d42129ac5cd188f08c1970b9d08e804c48a9
gahcep man <promdev@gmail.com>
2011-10-06 01:20:55 +1100

    Base frame of hook class is done. Class is processing only PrintScreen key pressing. Checkings are absent

--- source/KeyHooks/KeyHook/KeyHook.h
+++ source/KeyHooks/KeyHook/KeyHook.h
@@ -1,8 +1,59 @@
 #pragma once
-class KeyHook
+#include <windows.h>
+#include "../KeyHookHandler/KeyHookHandler.h"
+
+//#ifdef EXPORT_PURPOSE
+//   #define DLL_CLASS_PURPOSE    __declspec(dllexport)
+//#else
+//   #define DLL_CLASS_PURPOSE    __declspec(dllimport)
+//#endif
+
+class __declspec(dllexport) KeyHook
 {
+	/* VARIABLES */
+
 public:
-	KeyHook(void);
-	~KeyHook(void);
+
+	static KeyHook* g_KeyHookThisPointer;
+	
+	static const unsigned int POSITIVENUM_RETURN = 100;
+
+	enum EKEYS
+	{
+		KEY_ALL		= ~0,
+		KEY_PRTSCR	= 1,
+		KEY_ESCAPE	= 2,
+		KEY_CONTROL = 4,
+		KEY_MENU	= 8,
+		KEY_SHIFT	= 16
+	};
+
+	EKEYS m_ekKeysToTrack;
+
+	HHOOK m_hhKeyHook;
+
+
+private:
+
+	KeyHookHandler * m_khhHandler;
+	
+	/* METHODS */
+
+public:
+	
+	KeyHook(KeyHookHandler * p_khhHandler);
+
+	void KeyHookInstall();
+
+	void SetKeysToTrack(EKEYS p_ekKeys);
+
+	void KeyHookUninstall();
+
+	~KeyHook();
+
+private:
+
+	static LRESULT CALLBACK SnapProcedure(int iCode, WPARAM wParam, LPARAM lParam);
 };
 
+KeyHook* KeyHook::g_KeyHookThisPointer = 0;
