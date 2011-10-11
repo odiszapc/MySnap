60b070eea62037ce27b4b13d961f75320ff850cc
gahcep man <promdev@gmail.com>
2011-10-11 23:58:50 +1100

    This is alpha release. Now works all combinations: PrtSc, PrtSc+ALT, PrtSc+Shift, PrtSc+Ctrl

--- source/KeyHooks/KeyHook/KeyHook.cpp
+++ source/KeyHooks/KeyHook/KeyHook.cpp
@@ -5,6 +5,8 @@
 KeyHook::KeyHook(KeyHookHandler * p_khhHandler)
 {
 	m_khhHandler = p_khhHandler;
+
+	m_ekKeysToTrack = EKEYS::KEY_NONE;
 }
 
 void KeyHook::KeyHookInstall()
@@ -24,10 +26,93 @@ void KeyHook::KeyHookUninstall()
 	UnhookWindowsHookEx(m_hhKeyHook);
 }
 
-void KeyHook::SetKeysToTrack(EKEYS p_ekKey)
+bool KeyHook::SetKeyCallback(unsigned int p_Key, INVOKECALLBACK p_Callback, bool p_isOverwrite)
 {
-	m_ekKeysToTrack = p_ekKey;
+	// Exit by failure if we have read-only duplicates
+	if (BindCheckExistence(p_Key) && !p_isOverwrite)
+	{
+		m_LastError = KH_KEYDUPLICATE;
+		return false;
+	}
+		
+	BindAddKeyCallback(p_Key, p_Callback);
+	return true;
+}
+
+bool KeyHook::DelKeyCallback(unsigned int p_Key)
+{
+	if (BindCheckExistence(p_Key))
+	{
+		BindDeleteKeyCallback(p_Key);
+		return true;
+	}
 	
+	m_LastError = KH_KEYMISSING;
+
+	return false;
+}
+
+// Return true, if either key is duplicated
+bool KeyHook::BindCheckExistence(unsigned int p_Key)
+{
+	// Ckeck every bit in p_Key;
+	return (m_ekKeysToTrack & p_Key) > 0;
+}
+
+// If callback for selected key exists, owerwrite it
+void KeyHook::BindAddKeyCallback(unsigned int p_Key, INVOKECALLBACK p_Callback)
+{
+	// Add new keys in global var
+	m_ekKeysToTrack = m_ekKeysToTrack | p_Key;
+
+	// Add callback
+	m_arInvokeCallbacks.push_back(make_pair(p_Key, p_Callback));
+}
+
+// Deleting only key's callback, but key itself stays untouchable
+void KeyHook::BindDeleteKeyCallback(unsigned int p_Key)
+{
+	auto it = m_arInvokeCallbacks.begin();
+
+	for (; it != m_arInvokeCallbacks.end(); it++)
+	{
+		if (it->first == p_Key)
+		{
+			m_arInvokeCallbacks.erase(it);
+			m_ekKeysToTrack = (EKEYS) (m_ekKeysToTrack & (~p_Key));
+			break;
+		}
+	}
+}
+
+int KeyHook::RetrieveCallbackID(EKEYS p_Key)
+{
+	auto it = m_arInvokeCallbacks.begin();
+
+	for (int iCal = 0; it != m_arInvokeCallbacks.end(); it++, iCal++)
+	{
+		if (it->first & p_Key)
+		{
+			m_LastError = KH_SUCCESS;
+			return iCal;
+		}
+	}
+
+	m_LastError = KH_KEYMISSING;
+	return -1;
+}
+
+BOOL KeyHook::InvokeCallback(EKEYS p_Key)
+{
+	int l_ID = g_KeyHookThisPointer->RetrieveCallbackID(p_Key);
+					
+	// This is not supposed to happen (due to SetKeyCallback's duplicate verify)
+	if (l_ID == -1)	
+		throw new exception("InvokeCallback::Error=\"KEY is missing\"", KHE_INCORRECT_ID);
+
+	INVOKECALLBACK l_Callback = m_arInvokeCallbacks[l_ID].second;
+
+	return (m_khhHandler->*l_Callback)();
 }
 
 LRESULT CALLBACK KeyHook::SnapProcedure(int iCode, WPARAM wParam, LPARAM lParam)
@@ -36,21 +121,104 @@ LRESULT CALLBACK KeyHook::SnapProcedure(int iCode, WPARAM wParam, LPARAM lParam)
 
 	BYTE kbStatus[256];
 
-	// Is keystroke? 
-	if (iCode == HC_ACTION)
+	// Is it a keystroke and keydown? 
+	if ((iCode == HC_ACTION) && ((wParam == WM_KEYDOWN) || ((wParam == WM_SYSKEYDOWN))))
 	{
-		// Analyze m_ekKeysToTrack
-		// ...
+		// Process KEY_NONE situation
+		if (g_KeyHookThisPointer->m_ekKeysToTrack == EKEYS::KEY_NONE)
+			return CallNextHookEx(0, iCode, wParam, lParam);
 
 		hStruct = reinterpret_cast<LPKBDLLHOOKSTRUCT>(lParam);
-
-		// King of button pressed
+		
+		// PRTSC was pressed
 		if (hStruct->vkCode == VK_SNAPSHOT)
 		{
-			KeyHook::g_KeyHookThisPointer->m_khhHandler->OnPrintScreen();
-			
-			// Returning any positive value to prevent futher hook chain propogation
-			return POSITIVENUM_RETURN;
+			// Check if CTRL was pressed
+			if (GetAsyncKeyState(VK_CONTROL)>>((sizeof(SHORT) * 8) - 1))
+			{
+				// if CTRL+PRTSC should be checked
+				if ((g_KeyHookThisPointer->m_ekKeysToTrack & EKEYS::KEY_PRTSCR_CONTROL) > 0)
+				{
+					//MessageBox(0, "CTRL+PRTSC", "sd", 0);
+
+					BOOL l_InvokeRes;
+					try
+					{
+						l_InvokeRes = g_KeyHookThisPointer->InvokeCallback(EKEYS::KEY_PRTSCR_CONTROL);
+					}
+					catch(exception e)
+					{
+						// stage further
+						throw new exception(e);
+					}
+
+					return POSITIVE_NUM_RETURN;
+				}
+			}
+			else
+			// Check if SHIFT was pressed
+			if (GetAsyncKeyState(VK_SHIFT)>>((sizeof(SHORT) * 8) - 1))
+			{
+				// if SHIFT+PRTSC should be checked
+				if ((g_KeyHookThisPointer->m_ekKeysToTrack & EKEYS::KEY_PRTSCR_SHIFT) > 0)
+				{
+					//MessageBox(0, "SHIFT+PRTSC", "sd", 0);
+
+					BOOL l_InvokeRes;
+					try
+					{
+						l_InvokeRes = g_KeyHookThisPointer->InvokeCallback(EKEYS::KEY_PRTSCR_SHIFT);
+					}
+					catch(exception e)
+					{
+						// stage further
+						throw new exception(e);
+					}
+
+					return POSITIVE_NUM_RETURN;
+				}
+			}
+			else
+			// Check if ALT was pressed
+			if (GetAsyncKeyState(VK_MENU)>>((sizeof(SHORT) * 8) - 1))
+			{
+				// if ALT+PRTSC should be checked
+				if ((g_KeyHookThisPointer->m_ekKeysToTrack & EKEYS::KEY_PRTSCR_MENU) > 0)
+				{
+					//MessageBox(0, "ALT+PRTSC", "sd", 0);
+
+					BOOL l_InvokeRes;
+					try
+					{
+						l_InvokeRes = g_KeyHookThisPointer->InvokeCallback(EKEYS::KEY_PRTSCR_MENU);
+					}
+					catch(exception e)
+					{
+						// stage further
+						throw new exception(e);
+					}
+
+					return POSITIVE_NUM_RETURN;
+				}
+			}
+			else
+			{
+				//MessageBox(0, "PRTSC", "sd", 0);
+
+				BOOL l_InvokeRes;
+				try
+				{
+					l_InvokeRes = g_KeyHookThisPointer->InvokeCallback(EKEYS::KEY_PRTSCR_MENU);
+				}
+				catch(exception e)
+				{
+					// stage further
+					throw new exception(e);
+				}
+
+				// Returning any positive value to prevent futher hook chain propogation
+				return POSITIVE_NUM_RETURN;
+			}
 		}
 	}
 
--- source/KeyHooks/KeyHook/KeyHook.h
+++ source/KeyHooks/KeyHook/KeyHook.h
@@ -1,5 +1,12 @@
 #pragma once
+
+// standard's
 #include <windows.h>
+
+// STL's
+#include <vector>
+
+// project's
 #include "../KeyHookHandler/KeyHookHandler.h"
 
 //#ifdef EXPORT_PURPOSE
@@ -8,50 +15,93 @@
 //   #define DLL_CLASS_PURPOSE    __declspec(dllimport)
 //#endif
 
+using namespace std;
+
+
+enum EKEYS
+	{
+		KEY_ALL				= ~0,
+		KEY_NONE			= 0,
+		KEY_PRTSCR			= 1,
+		KEY_PRTSCR_CONTROL	= 2,
+		KEY_PRTSCR_MENU		= 4,
+		KEY_PRTSCR_SHIFT	= 8		
+	};
+
+
 class __declspec(dllexport) KeyHook
 {
+	/* CONSTANTS */
+
+	// Callback definition
+	typedef BOOL (KeyHookHandler::*INVOKECALLBACK) (void);
+	//INVOKECALLBACK m_khkCallback;
+
+	// Constants
+	static const unsigned int POSITIVE_NUM_RETURN = 100;
+
+	// Error codes
+	static const unsigned int KH_SUCCESS		= 0;
+	static const unsigned int KH_KEYDUPLICATE	= 1;
+	static const unsigned int KH_KEYMISSING		= 2;
+
+	// Exception codes
+	static const unsigned int KHE_INCORRECT_ID	= 100;
+
+
 	/* VARIABLES */
 
 public:
 
 	static KeyHook* g_KeyHookThisPointer;
 	
-	static const unsigned int POSITIVENUM_RETURN = 100;
-
-	enum EKEYS
-	{
-		KEY_ALL		= ~0,
-		KEY_PRTSCR	= 1,
-		KEY_ESCAPE	= 2,
-		KEY_CONTROL = 4,
-		KEY_MENU	= 8,
-		KEY_SHIFT	= 16
-	};
-
-	EKEYS m_ekKeysToTrack;
+	unsigned int m_ekKeysToTrack;
 
 	HHOOK m_hhKeyHook;
+
+	#pragma region
+	#pragma endregion
 
 
 private:
 
 	KeyHookHandler * m_khhHandler;
 	
+	// vector of key-callback bindings
+	vector<pair<unsigned int, INVOKECALLBACK>> m_arInvokeCallbacks;
+
+	unsigned int m_LastError;
+
+
 	/* METHODS */
 
 public:
 	
 	KeyHook(KeyHookHandler * p_khhHandler);
 
+	unsigned int GetLastError() { return m_LastError; };
+
 	void KeyHookInstall();
-
-	void SetKeysToTrack(EKEYS p_ekKeys);
-
 	void KeyHookUninstall();
 
+	unsigned int GetTrackedKeys() { return m_ekKeysToTrack; };
+
+	bool SetKeyCallback(unsigned int p_Key, INVOKECALLBACK p_Callback, bool p_isOverwrite);
+	bool DelKeyCallback(unsigned int _Key);
+	
 	~KeyHook();
 
 private:
+
+	// Private inner service functions, working only with callback bindings
+	bool BindCheckExistence(unsigned int p_Key);
+	void BindAddKeyCallback(unsigned int p_Key, INVOKECALLBACK p_Callback);
+	void BindDeleteKeyCallback(unsigned int p_Key);
+
+	// Invoke chosen callback, call RetrieveCallbackID function
+	BOOL InvokeCallback(EKEYS p_Key);
+	// Retrieve vector's number of required key callback
+	int RetrieveCallbackID(EKEYS p_Key);
 
 	static LRESULT CALLBACK SnapProcedure(int iCode, WPARAM wParam, LPARAM lParam);
 };
